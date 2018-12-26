df16d40d177127cf3b8454ab17e54a95bc5255c5
Swerad <lukasz.sitnik@gmail.com>
2018-12-26 21:52:53 +0100

    "keyboard.js" class/module created

---
+++ js/keyboard.js
@@ -0,0 +1,16 @@
+const keyboard = (() => {
+    let divContent = "";
+    const drawKeys = () => {
+        for (i = 0; i < 35; i++) {
+
+            let element = "fig" + i;
+            divContent = divContent + `<div class="litera" onclick="check('${i}')" id="${element}">${figures[i]} </div>`;
+            if ((i + 1) % 7 == 0) divContent = divContent + `<div style="clear: both;"></div>`;
+        }
+        return divContent;
+    }
+
+    return {
+        drawKeys: drawKeys
+    }
+})()
