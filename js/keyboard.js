5dfb3a70200d08cb39c199f38326c76c62ddb9a3
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2019-01-07 01:56:50 +0100

    Code responsible for arranging letters in rows wrapped in function 'arrangeKeys'

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,19 +1,23 @@
-const keyboard = (() => {
-    const drawKeys = () => {
-        let divContent = "";
-        const alphabet = new Figures();
-        const alphabetLength = alphabet.figuresLength;
-        const figures = alphabet.figures;
-        for (i = 0; i < alphabetLength; i++) {
-
-            let element = "fig" + i;
-            divContent = divContent + `<div class="litera" onclick="check('${i}')" id="${element}">${figures[i]} </div>`;
-            if ((i + 1) % 7 == 0) divContent = divContent + `<div style="clear: both;"></div>`;
-        }
-        return divContent;
-    }
-
-    return {
-        drawKeys: drawKeys
-    }
-})()
+  const keyboard = (() => {
+      const drawKeys = () => {
+          let divContent = "";
+          const alphabet = new Figures();
+          const alphabetLength = alphabet.figuresLength;
+          const figures = alphabet.figures;
+         for (i = 0; i < alphabetLength; i++) {
+              let element = "fig" + i;
+              divContent += `<div class="litera" onclick="check('${i}')" id="${element}">${figures[i]} </div>`;
+              const arrangeKeys = index => {
+                const figuresInRow = 7;
+                const cuttingDiv = `<div style="clear: both;"></div>`;
+                if ((index + 1) % figuresInRow === 0) divContent += cuttingDiv;
+              }
+              arrangeKeys(i);
+          }
+          return divContent;
+      }
+  
+      return {
+          drawKeys: drawKeys
+      }
+  })()
