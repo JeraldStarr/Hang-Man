43b95f50b3ebd4cdc91532f755fc9c34e9e23d42
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2019-01-11 00:56:17 +0100

    Class 'Keyboard' created

---
+++ script/Keyboard.js
@@ -0,0 +1,41 @@
+class Keyboard {
+    constructor() {
+        this.figures = ["A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N", "Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U", "V", "W", "X", "Y", "Z", "Ż", "Ź"];
+    }
+    drawKeys() {
+        const alphabetContainer = document.querySelector('#alphabet');
+        const createDivHTML = () => {
+            const divHTML = document.createElement("div");
+            return divHTML;
+        }
+        const createKeyHTML = (id, index) => {
+            const divHTML = createDivHTML();
+            divHTML.classList.add("letter");
+            divHTML.id = id;
+            divHTML.textContent = this.figures[index];
+            return divHTML;
+        }
+        for (let i = 0; i < this.figures.length; i++) {
+            let elementId = "fig" + i;
+            const arrangeKeys = index => {
+                const figuresInRow = 7;
+                const cuttingDiv = (function() {
+                    const divHTML = createDivHTML();
+                    divHTML.setAttribute("style", "clear: both");
+                    return divHTML;
+                })();
+                alphabetContainer.appendChild(createKeyHTML(elementId, i));
+                if ((index + 1) % figuresInRow === 0) alphabetContainer.appendChild(cuttingDiv);
+            }
+            arrangeKeys(i);
+        }
+        return alphabetContainer;
+    }
+    onClick(figures) {
+        [...figures.childNodes].forEach((figure) => {
+            figure.addEventListener("click", () => {
+                validator.validatePassword(figure.textContent);
+            });
+        });
+    }
+}
