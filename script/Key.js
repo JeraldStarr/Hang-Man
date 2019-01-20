60f7b0250f9ce10ab989426deaca26b7fd002840
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2019-01-20 22:45:42 +0100

    Changed class name from 'HTML' to 'Key' and refactored

--- script/Key.js
+++ script/Key.js
@@ -1,12 +1,8 @@
-class HTML {
+class Key {
     constructor() {
-        this.alphabetHTML = this.get("#alphabet");
-        this.boardHTML = this.get("#board");
-        this.hangmanImage = this.get("#szubienica");
-        this.keyboard;
-    }
-    get(element) {
-        return document.querySelector(element);
+        this.alphabetHTML = document.querySelector("#alphabet");
+        this.boardHTML = document.querySelector("#board");
+        this.hangmanImage = document.querySelector("#szubienica");
     }
     createKey(id, index) {
         const divHTML = document.createElement("div");
@@ -22,24 +18,19 @@ class HTML {
         divHTML.setAttribute("style", "clear: both");
         return divHTML;
     }
-    addKeysToView(id, index) {
-        alphabet.appendChild(this.createKey(id, index));
+    addKeysToView(id, index, alphabetContainer) {
+        alphabetContainer.appendChild(this.createKey(id, index));
     }
-    addCuttingDivToView() {
-        alphabet.appendChild(this.createCuttingDiv());
-    }
-    arrangeKeys(index, id) {
-        const figuresInRow = 7;
-        this.addKeysToView(id, index);
-        if ((index + 1) % figuresInRow === 0) this.addCuttingDivToView(alphabet);
+    addCuttingDivToView(alphabetContainer) {
+        alphabetContainer.appendChild(this.createCuttingDiv());
     }
     setID(i) {
         return "fig" + i;   
     }
-    buildKeys() {
+    buildKeys(keyboard) {
         const alphabetLenght = game.keyboard.getFigures().length;
         for (let i = 0; i < alphabetLenght; i++) {
-            this.arrangeKeys(i, this.setID(i));
+            keyboard.arrangeKeys(i, this.setID(i));
         }
     }
 }
