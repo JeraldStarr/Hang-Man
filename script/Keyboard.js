2a31e3c0564ddecaaac7a3b86ad6fcffb70d4238
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2019-01-20 23:06:29 +0100

    'Keyboard' class refactored

--- script/Keyboard.js
+++ script/Keyboard.js
@@ -1,23 +1,34 @@
 class Keyboard {
     constructor() {
-        this.figures = ["A", "Ą", "B", "C", "Ć", "D",
-                        "E", "Ę", "F", "G", "H", "I",
-                        "J", "K", "L", "Ł", "M", "N",
-                        "Ń", "O", "Ó", "P", "Q", "R",
-                        "S", "Ś", "T", "U", "V", "W",
-                        "X", "Y", "Z", "Ż", "Ź"];
+        this.key = new Key();
+        this.alphabetContainer = document.querySelector('#alphabet');
+    }
+    getFigures() {
+        const figures = ["A", "Ą", "B", "C", "Ć", "D",
+                         "E", "Ę", "F", "G", "H", "I",
+                         "J", "K", "L", "Ł", "M", "N",
+                         "Ń", "O", "Ó", "P", "Q", "R",
+                         "S", "Ś", "T", "U", "V", "W",
+                         "X", "Y", "Z", "Ż", "Ź"];
+        return figures;
+    }
+    arrangeKeys(index, id) {
+        const figuresInRow = 7;
+        this.key.addKeysToView(id, index, this.alphabetContainer);
+        if ((index + 1) % figuresInRow === 0) 
+        this.key.addCuttingDivToView(this.alphabetContainer);
     }
     drawKeys() {
-        const html = new HTML();
-        const alphabetContainer = html.getHTML('#alphabet');
-        html.buildHTML();
-        return alphabetContainer;
+        this.key.buildKeys(this);
     }
-    onClick(alphabetContainer) {
-        [...alphabetContainer.childNodes].forEach((key) => {
-            key.addEventListener("click", () => {
-                validator.validatePassword(key.textContent);
-            });
+    onClick() {
+        [...this.alphabetContainer.childNodes].forEach((key) => {
+            if (key.id) {
+                key.addEventListener("click", () => {
+                    // validator.validatePassword(key.textContent);
+                });
+            }
         });
     }
 }
+
