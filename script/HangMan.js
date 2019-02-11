399b18d8cc9fdf3fdc31c43e68e0504e616e5802
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2019-02-11 21:38:04 +0100

    New private method '_showFailedMessage' in 'hangMan' class

--- script/HangMan.js
+++ script/HangMan.js
@@ -1,14 +1,12 @@
 class HangMan {
-    constructor() {
+    constructor(showFailedMessage) {
         this._fails = 0;
         this._hangManImage = document.querySelector("#gibbet img");
+        this._showFailedMessage = showFailedMessage;
         this._failed = () => {
-            const alphabetContainer = document.querySelector("#alphabet");
              const NUMBER_OF_FAILS = 9;
              if (this._fails >= NUMBER_OF_FAILS) {
-                 alphabetContainer.innerHTML = `Przegrana! Prawidłowe hasło: 
-                 ${game.password._selectedPassword} <br/><br/><span class="reset" 
-                 onclick="location.reload()">JESZCZE RAZ?</span>`;
+                this._showFailedMessage();
              }
          }
     }
